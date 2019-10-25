4248bd711099a7b4e225b4f0ad00658baaf9b749
s40723247 <40723247@gm.nfu.edu.tw>
2019-10-25 13:51:28 +0800

    123

--- init.py
+++ init.py
@@ -10,7 +10,7 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "s40723247 cad2019"
+    site_title = " s40723247 cad2019 "
     def __init__(self):
         # hope to create downloads and images directories　
         if not os.path.isdir(_curdir + "/downloads"):
